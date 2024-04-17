9765f5a8fbd96b070d2abdedd2800d64c6c22eef
Anshuman Tripathy <swayamshree16@gmail.com>
2024-04-17 19:52:58 +0530

    update

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -3,36 +3,24 @@ import { Table } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import './Graph.css';
 
-/**
- * Props declaration for <Graph />
- */
 interface IProps {
-  data: ServerRespond[],
+ data: ServerRespond[],
 }
 
-/**
- * Perspective library adds load to HTMLElement prototype.
- * This interface acts as a wrapper for Typescript compiler.
- */
-interface PerspectiveViewerElement {
-  load: (table: Table) => void,
+interface PerspectiveViewerElement extends HTMLDivElement {
+ perspective: any; // tslint:disable-line:no-any
+ load: (table: Table) => void,
 }
 
-/**
- * React component that renders Perspective based on data
- * parsed from its parent through data property.
- */
 class Graph extends Component<IProps, {}> {
-  // Perspective table
-  table: Table | undefined;
+ table: Table | undefined;
 
-  render() {
+ render() {
     return React.createElement('perspective-viewer');
-  }
+ }
 
-  componentDidMount() {
-    // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+ componentDidMount() {
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -45,20 +33,20 @@ class Graph extends Component<IProps, {}> {
       this.table = window.perspective.worker().table(schema);
     }
     if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
-
-      // Add more Perspective configurations here.
       elem.load(this.table);
-    }
-  }
 
-  componentDidUpdate() {
-    // Everytime the data props is updated, insert the data into Perspective table
+      // Set additional attributes on the perspective-viewer element
+      elem.setAttribute('view', 'hypergrid'); // Example view, adjust as needed
+      elem.setAttribute('column-pivots', '["stock"]'); // Example column-pivots, adjust as needed
+      elem.setAttribute('row-pivots', '["timestamp"]'); // Example row-pivots, adjust as needed
+      elem.setAttribute('columns', '["stock", "top_ask_price", "top_bid_price", "timestamp"]'); // Example columns, adjust as needed
+      elem.setAttribute('aggregates', '{"top_ask_price": "avg", "top_bid_price": "avg"}'); // Example aggregates, adjust as needed
+    }
+ }
+
+ componentDidUpdate() {
     if (this.table) {
-      // As part of the task, you need to fix the way we update the data props to
-      // avoid inserting duplicated entries into Perspective table again.
       this.table.update(this.props.data.map((el: any) => {
-        // Format the data from ServerRespond to the schema
         return {
           stock: el.stock,
           top_ask_price: el.top_ask && el.top_ask.price || 0,
@@ -67,7 +55,7 @@ class Graph extends Component<IProps, {}> {
         };
       }));
     }
-  }
+ }
 }
 
 export default Graph;
